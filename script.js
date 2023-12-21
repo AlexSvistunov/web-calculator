c927bc9e4cb4218b0d451e9764d8522436587dc2
Alexey Svistunov <xdayx53@gmail.com>
2023-12-21 12:29:48 +0300

    added html + css (apearance of the calculator)

---
+++ script.js
@@ -0,0 +1,31 @@
+const inputField = document.querySelector('.calculator__input')
+const numberButtons = document.querySelectorAll('.calculator__numbers-number')
+const functionButtons = document.querySelectorAll('.calculator__functions-function')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//чтобы выражения можно было записывать и в строчку
+//в выпадающем меню другие калькуляторы
+
+//превью как у калькулятора windows
